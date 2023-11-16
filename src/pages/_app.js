bbf1412c731e62392a5d5fb1913a5a2e9d1e88b3
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-16 10:26:36 -0400

    Set up `App` component

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import '../app/styles/globals.css';
+import '../app/styles/footer.css';
+import '../app/styles/navbar.css';
+
+import Navbar from '../app/components/Navbar';
+import Footer from '../app/components/Footer';
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
