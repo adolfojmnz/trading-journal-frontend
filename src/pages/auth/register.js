3d766931bba6219e56d161827f61dcd34d72d4e4
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-16 10:16:26 -0400

    Add sample registration page

---
+++ src/pages/auth/register.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform registration logic here
+    console.log('Registration submitted:', name, email, password);
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Register</h1>
+      <form onSubmit={handleSubmit} className="w-64">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-lg font-semibold mb-2">Name:</label>
+          <input
+            type="text"
+            id="name"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-lg font-semibold mb-2">Email:</label>
+          <input
+            type="email"
+            id="email"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-lg font-semibold mb-2">Password:</label>
+          <input
+            type="password"
+            id="password"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
