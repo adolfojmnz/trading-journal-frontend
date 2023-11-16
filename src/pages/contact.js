8cff836fbd17bea8a76f045ed40166468392de23
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-16 10:13:08 -0400

    Add sample contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+const ContactPage = () => {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+    console.log('Form submitted:', email, subject, message);
+    // Reset form fields
+    setEmail('');
+    setSubject('');
+    setMessage('');
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
+      <p className="text-lg mb-6">
+        We would love to hear from you! If you have any questions or inquiries, please feel free to reach out to us using the contact form below:
+      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-lg font-semibold mb-2">Email:</label>
+          <input
+            type="email"
+            id="email"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="subject" className="block text-lg font-semibold mb-2">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-lg font-semibold mb-2">Message:</label>
+          <textarea
+            id="message"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            rows="4"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
