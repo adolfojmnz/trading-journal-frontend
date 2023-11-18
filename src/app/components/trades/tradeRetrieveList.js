18b4c5710b14b1ad1a5a1f0e523bc3b24dd37c2b
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-18 19:03:10 -0400

    Improves the style of the rendered trade list

--- src/app/components/trades/tradeRetrieveList.js
+++ src/app/components/trades/tradeRetrieveList.js
@@ -38,34 +38,34 @@ const TradeRetrieveList = () => {
         <table className="trade-list w-full border-collapse">
           <thead>
             <tr className="bg-gray-200">
-              <th className="py-2 px-4 border-b text-left w-1/9">Ticket</th>
-              <th className="py-2 px-4 border-b text-left w-2/9">Currency Pair</th>
-              <th className="py-2 px-4 border-b text-left w-1/9">Type</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Open DateTime</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Close DateTime</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Open Price</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Take Profit</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Stop Loss</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Close Price</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">Volume</th>
-              <th className="py-2 px-4 border-b text-right w-1/9">PnL</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Ticket</th>
+              <th className="py-2 px-4 border-b text-center w-2/9">Currency Pair</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Type</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Open DateTime</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Close DateTime</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Open Price</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Take Profit</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Stop Loss</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Close Price</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">Volume</th>
+              <th className="py-2 px-4 border-b text-center w-1/9">PnL</th>
             </tr>
           </thead>
           <tbody>
             {trades.length > 0 ? (
               trades.map((trade, index) => (
                 <tr onClick={() => handlerowClick(trade.id)} className="trade-list-item" key={index}>
-                  <td className="py-2 px-4 border-b text-left w-1/9">{trade.ticket}</td>
-                  <td className="py-2 px-4 border-b text-left w-2/9">{trade.currency_pair}</td>
-                  <td className="py-2 px-4 border-b text-left w-1/9">{trade.type === "S" ? "Short" : "Long"}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.open_datetime}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.close_datetime}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.open_price}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.take_profit}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.stop_loss}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.close_price}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.volume}</td>
-                  <td className="py-2 px-4 border-b text-right w-1/9">{trade.pnl}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.ticket}</td>
+                  <td className="py-2 px-4 border-b text-center w-2/9">{trade.currency_pair}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.type === "S" ? "Short" : "Long"}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.open_datetime}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.close_datetime}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.open_price}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.take_profit}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.stop_loss}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.close_price}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.volume}</td>
+                  <td className="py-2 px-4 border-b text-center w-1/9">{trade.pnl}</td>
                 </tr>
               ))
             ) : (
