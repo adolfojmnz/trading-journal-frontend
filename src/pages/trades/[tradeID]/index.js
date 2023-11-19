45c7df1d1f4de5479f64e3368ef0f0785116f19a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-19 14:35:37 -0400

    Refacor: place trade related styles in `[...]/styles/trades/`

--- src/pages/trades/[tradeID]/index.js
+++ src/pages/trades/[tradeID]/index.js
@@ -1,8 +1,8 @@
-import '../../../app/styles/tradeDetails.css';
+import '../../../app/styles/trades/retrieve.css';
 import RetrieveTrade from '@/app/components/trades/retrieve';
 
 
-export default function TradeListPage() {
+export default function TradeDetailsPage() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <main>
--- src/pages/trades/index.js
+++ src/pages/trades/index.js
@@ -1,4 +1,4 @@
-import '../../app/styles/tradeList.css';
+import '../../app/styles/trades/list.css';
 import ListTrades from '@/app/components/trades/list';
 
 
