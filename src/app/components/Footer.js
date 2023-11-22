1a8de8064a4f42fa94a7648a691e7d22394cb7b4
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-22 10:19:34 -0400

    Remove dark background

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 flex-shrink-0">
+    <footer className="flex-shrink-0">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-gray-300 text-sm">
+        <p className="text-center text-black text-xl-3">
           &copy; {new Date().getFullYear()} Adolfo Jimenez. All rights reserved.
         </p>
       </div>
