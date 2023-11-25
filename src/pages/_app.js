f5d0daad3a6736769303aff16ef50b0ecd409ef5
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-25 12:12:55 -0400

    Remove Footer ...
    This commit removes the footer as it requires further work
    to make it properly work.
    
    The footer will be add in the future and it shall include
    links and some usefull information.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,12 @@
 import "../app/styles/globals.css";
 
 import Navbar from "../app/components/Navbar";
-import Footer from "../app/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
